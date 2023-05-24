a71171bf40f633debd5e7d9e0f2dac874a098eab
BatoolAlsugaih <batolahmad.2004@gmail.com>
2023-05-24 21:49:35 +0300

    testing GitHub

---
+++ firstR.java
@@ -0,0 +1,8 @@
+public class firstR {
+public static void main(String[] args) {
+for (int i=0 ; i<=10; i++){
+System.out.println(" testing GitHub ");
+System.out.println("Batool's first Repository ");
+}    
+}
+}
